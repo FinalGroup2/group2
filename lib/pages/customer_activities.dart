38ff0244e57a43fdf8fefd5a67cbdff75c578846
Muthahhar97 <muthahharrdx@hotmail.com>
2022-05-22 07:19:34 +0530

    cards round edges

--- lib/pages/customer_activities.dart
+++ lib/pages/customer_activities.dart
@@ -42,6 +42,9 @@ class _CActivityState extends State<CActivity> {
               ),
             ),
             Card(
+              shape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.circular(20),
+              ),
               elevation: 14.0,
               shadowColor: Color(0x802196F3),
               margin: EdgeInsets.fromLTRB(30.0, 20.0, 30.0,10.0),
@@ -122,7 +125,9 @@ class _CActivityState extends State<CActivity> {
                   itemCount: preActivities.length,
                   itemBuilder: (context,index){
                     return Card(
-
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(10.0),
+                      ),
                       elevation: 10.0,
                       shadowColor: Colors.blueAccent,
                       margin: EdgeInsets.fromLTRB(10.0, 0,10.0,10.0),
